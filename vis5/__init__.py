c8c9ab70e902744398c184b4903b2d7d6f2c32bb
Richard Shaw <richard@phas.ubc.ca>
2021-03-08 09:53:23 -0800

    Fix linter issues

--- vis5/__init__.py
+++ vis5/__init__.py
@@ -1,6 +1,5 @@
-# your_library/__init__.py
+"""vis5 data format"""
+
 import dunamai as _dunamai
 
-__version__ = _dunamai.get_version(
-    "vis5", third_choice=_dunamai.Version.from_any_vcs
-)
+__version__ = _dunamai.get_version("vis5", third_choice=_dunamai.Version.from_any_vcs)
